0cba055664eeeae666069926382a88d384cd32a9
shahzad-eth <200927579+shahzad-eth@users.noreply.github.com>
2026-08-07 18:23:02 +0530

    fix: typo in the multer callback.

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -2,11 +2,11 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cd(null, "./public/temp");
+    cb(null, "./public/temp");
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cd(null, file.originalname + "-" + uniqueSuffix);
+    cb(null, file.originalname + "-" + uniqueSuffix);
   },
 });
 
