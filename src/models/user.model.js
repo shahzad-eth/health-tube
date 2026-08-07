3a85ef9a11f5fa79107d9934824aff8365c69c5f
shahzad-eth <200927579+shahzad-eth@users.noreply.github.com>
2026-08-07 15:12:58 +0530

    feat: Add user model methods. - Define hash password method. - Define JWT tokens methods.

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema(
     {
@@ -41,5 +43,37 @@ const userSchema = new Schema(
     },
     { timestamps: true }
 )
+
+userSchema.methods("save", async function (next) {
+    if (!this.modified("password")) return next();
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = async function (username, email,) {
+    jwt.sign(
+        {
+            _id= this._id,
+            email = this.email,
+            username = this.username,
+        }, process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+    )
+}
+
+userSchema.methods.generateRefreshToken = async function (username, email,) {
+    jwt.sign(
+        {
+            _id= this._id,
+            email = this.email,
+            username = this.username,
+        }, process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+    )
+}
 
 export const User = mongoose.model("User", userSchema)
