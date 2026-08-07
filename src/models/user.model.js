961e34388d348ae550ffe3eb923ef0b09c4c4d74
shahzad-eth <200927579+shahzad-eth@users.noreply.github.com>
2026-08-07 21:29:33 +0530

    fix: Bugs in `user.model.js`.
    - Remove `next()` from pre-save hook.
    - fix missing `return` from jwt token methods.

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -44,37 +44,40 @@ const userSchema = new Schema(
   {timestamps: true},
 );
 
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  this.password = bcrypt.hash(this.password, 10);
-  next();
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+
+  this.password = await bcrypt.hash(this.password, 10);
 });
 
 userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
-userSchema.methods.generateAccessToken = async function (username, email) {
-  jwt.sign(
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
     {
       _id: this._id,
       email: this.email,
       username: this.username,
+      fullName: this.fullName,
     },
     process.env.ACCESS_TOKEN_SECRET,
-    {expiresIn: process.env.ACCESS_TOKEN_EXPIRY},
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    },
   );
 };
 
-userSchema.methods.generateRefreshToken = async function (username, email) {
-  jwt.sign(
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
     {
       _id: this._id,
-      email: this.email,
-      username: this.username,
     },
     process.env.REFRESH_TOKEN_SECRET,
-    {expiresIn: process.env.REFRESH_TOKEN_EXPIRY},
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    },
   );
 };
 
