a4fdf4fdbfba1a3de5b5f9cc6e73c6785653fec3
shahzad-eth <200927579+shahzad-eth@users.noreply.github.com>
2026-08-07 16:08:44 +0530

    fix: apply prettier format afte fixing syntax error.

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,79 +1,81 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose, {Schema} from "mongoose";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 const userSchema = new Schema(
-    {
-        username: {
-            type: String,
-            required: true,
-            lowercase: true,
-            trim: true,
-            unique: true,
-            index: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            lowercase: true,
-            trim: true,
-            unique: true,
-        },
-        fullName: {
-            type: String,
-            required: true,
-            trim: true,
-        },
-        avatar: {
-            type: String, // cloudinary URL
-            required: true
-        },
-        coverImage: String,
-        watchHistory: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: "Video"
-            }
-        ],
-        password: {
-            type: String,
-            required: [true, "Password is required"]
-        },
-        refreshToken: String
+  {
+    username: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+      unique: true,
+      index: true,
     },
-    { timestamps: true }
-)
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+      unique: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    avatar: {
+      type: String, // cloudinary URL
+      required: true,
+    },
+    coverImage: String,
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: String,
+  },
+  {timestamps: true},
+);
 
 userSchema.methods("save", async function (next) {
-    if (!this.modified("password")) return next();
-    this.password = bcrypt.hash(this.password, 10)
-    next()
-})
+  if (!this.modified("password")) return next();
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
 
 userSchema.methods.isPasswordCorrect = async function (password) {
-    return await bcrypt.compare(password, this.password)
-}
+  return await bcrypt.compare(password, this.password);
+};
 
-userSchema.methods.generateAccessToken = async function (username, email,) {
-    jwt.sign(
-        {
-            _id: this._id,
-            email: this.email,
-            username: this.username,
-        }, process.env.ACCESS_TOKEN_SECRET,
-        { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
-    )
-}
+userSchema.methods.generateAccessToken = async function (username, email) {
+  jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {expiresIn: process.env.ACCESS_TOKEN_EXPIRY},
+  );
+};
 
-userSchema.methods.generateRefreshToken = async function (username, email,) {
-    jwt.sign(
-        {
-            _id: this._id,
-            email: this.email,
-            username: this.username,
-        }, process.env.REFRESH_TOKEN_SECRET,
-        { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
-    )
-}
+userSchema.methods.generateRefreshToken = async function (username, email) {
+  jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {expiresIn: process.env.REFRESH_TOKEN_EXPIRY},
+  );
+};
 
-export const User = mongoose.model("User", userSchema)
+export const User = mongoose.model("User", userSchema);
