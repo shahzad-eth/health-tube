95aeae080d5f760f4fefc824deaed96a7f20a1d2
shahzad-eth <200927579+shahzad-eth@users.noreply.github.com>
2026-08-15 17:36:26 +0530

    delete: redundant file duration calculator file

--- src/utlils/get-video-duration.js
+++
@@ -1,9 +0,0 @@
-import {getVideoDurationInSeconds} from "get-video-duration";
-
-const getVideoDuration = (videoLocalPath) => {
-  const videoDuration = getVideoDurationInSeconds(videoLocalPath);
-
-  return videoDuration;
-};
-
-export {getVideoDuration}
