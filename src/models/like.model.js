da6709503b13025d5422de4518a30f1bb388ef0f
shahzad-eth <200927579+shahzad-eth@users.noreply.github.com>
2026-08-20 20:17:04 +0530

    fix(DB-models): remove multiple users in one like doc and fix typo in comment model
    - add unique indexes in like model

--- src/models/like.model.js
+++ src/models/like.model.js
@@ -14,14 +14,16 @@ const likeSchema = new Schema(
       type: Schema.Types.ObjectId,
       ref: "Tweet",
     },
-    likedBy: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-      },
-    ],
+    likedBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
   },
   {timestamps: true},
 );
+
+likeSchema.index({ video: 1, likedBy: 1 }, { unique: true, sparse: true });
+likeSchema.index({ comment: 1, likedBy: 1 }, { unique: true, sparse: true });
+likeSchema.index({ tweet: 1, likedBy: 1 }, { unique: true, sparse: true });
 
 export const Like = mongoose.model("Like", likeSchema);
